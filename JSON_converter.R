c5be0190188f93f08e31370fcd9f061a173e921b
Tyler Netherly <netherlyt@gmail.com>
2018-03-03 23:44:48 -0500

    I wrote the thing

--- JSON_converter.R
+++ JSON_converter.R
@@ -12,10 +12,21 @@ fips_codes <- read.csv("https://www2.census.gov/geo/docs/reference/codes/files/n
 # edate
 jsontoR <- function(FIPS,sdate,edate,elem) { # dates must be in YEAR-MONTH-DAY format
   library(jsonlite)
-  raw_json <- fromJSON(cat("http://data.rcc-acis.org/MultiStnData?county=",FIPS,"&sdate=",sdate,"&edate=",edate,"&elems=",elem))
+  raw_json <- fromJSON(paste("http://data.rcc-acis.org/MultiStnData?county=",FIPS,"&sdate=",sdate,"&edate=",edate,"&elems=",elem, sep = ""))
   initial_DF <- as.data.frame(raw_json) # Converts JSON into tree data frame
+  first_name <- initial_DF$data.meta$name[[1]]# Only want first station entry data
+  firststationvec <- unlist(initial_DF$data.data[initial_DF$data.meta$name == first_name])
+  date_range <- seq(as.Date(sdate),as.Date(edate),by='day')
+  my_df <- data.frame(days=date_range,first_name = firststationvec)
+  my_df$first_name[my_df$first_name == 'T'] <- NA
+  my_df$first_name[my_df$first_name == 'M'] <- NA
+  colnames(my_df)[2] <- paste(first_name,',',FIPS,sep = "")
+  my_df
   # Need to make a data frame with the first column be the date range and the other columns are the data
   # One column per sublist
   # JSON data is divided into two sublists data.meta and data.data
   # Each column has the name of an element in data.meta and contains the data for that element in data.data
 }
+df <- jsontoR(18139,"2010-01-01","2010-12-31","pcpn")
+df
+names(df)[2]
