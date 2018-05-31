065fb2252f535c2462ed610702a05466c80e5e28
Tyler Netherly <netherlyt@gmail.com>
2018-05-31 10:05:49 -0400

    I don't know how to write commit messages.

---
+++ download_all_json.R
@@ -0,0 +1,17 @@
+counties <- read.table('~/Desktop/national_county.txt',header = F, sep = ',', 
+                       fill = T, stringsAsFactors = F, quote = '')
+nrow(counties[counties$V1 == 'IN',])
+
+midwest_counties <- counties[counties$V1 == 'OH' | counties$V1 == 'IN' | counties$V1 == 'IL'
+                             | counties$V1 == 'IA' | counties$V1 == 'MO' | counties$V1 == 'KS' |
+                               counties$V1 == 'NE' | counties$V1 == 'SD' | counties$V1 == 'MN',]
+
+midwest_counties$V2 <- sprintf('%02d',midwest_counties$V2)
+midwest_counties$V3 <- sprintf('%03d',midwest_counties$V3)
+fips_code <- paste(midwest_counties$V2,midwest_counties$V3,sep = '')
+as.numeric(fips_code)
+class(fips_code)
+write.table(as.numeric(fips_code),file = '~/Desktop/fips_code.txt',sep = '\n')
+
+command <- function(x) system(paste('curl \'http://data.rcc-acis.org/MultiStnData?county=',x,'&sdate=1970-01-01&edate=2015-12-31&elems=pcpn&meta=ll\' > /scratch/mentors/dbuckmas/json_files/', x,'.json', sep = ''))
+command(46137)
