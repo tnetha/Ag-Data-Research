f8071e52025c81d4a0ad85027817611becb4e063
tydor73 <3710608+tydor73@users.noreply.github.com>
2018-06-11 12:09:19 -0400

    Added date column

--- importing_means.R
+++ importing_means.R
@@ -1,8 +1,5 @@
-test_mean <- read.csv('/scratch/mentors/dbuckmas/pcpn_means/17147.csv')
-View(test_mean)
-test_mean$X <- NULL
 
-pcpn_df <- data.frame(date = seq(as.Date('1970-01-01'),as.Date('2015-12-31'),by='days'))
+pcpn_dates <- data.frame(date = seq(as.Date('1970-01-01'),as.Date('2015-12-31'),by='days'))
 
 fips_code <- fips_code[-494]
 
@@ -11,5 +8,6 @@ pcpn_mat <- sapply(fips_code, function(x) read.csv(paste('/scratch/mentors/dbuck
 pcpn_df <- do.call(data.frame,pcpn_mat)
 pcpn_names <- paste('V',fips_code,sep = '')
 names(pcpn_df) <- pcpn_names
-head(pcpn_df)
-View(pcpn_df)
+
+pcpn_df <- cbind(pcpn_dates,pcpn_df)
+
