78d2bbff5c1571e1262f468f1724d8de54d4cd50
Madison Trout <trout2@purdue.edu>
2018-07-11 11:26:59 -0400

    l

--- download_all_json.R
+++ download_all_json.R
@@ -9,6 +9,7 @@ midwest_counties <- counties[counties$V1 == 'OH' | counties$V1 == 'IN' | countie
 midwest_counties$V2 <- sprintf('%02d',midwest_counties$V2)
 midwest_counties$V3 <- sprintf('%03d',midwest_counties$V3)
 fips_code <- paste(midwest_counties$V2,midwest_counties$V3,sep = '')
+fips_code <- fips_code[!fips_code %in% c(17087, 46113, 29019)]
 fips_code <- as.numeric(fips_code)
 
 pcpn_command <- function(x) system(paste('curl \'http://data.rcc-acis.org/MultiStnData?county=',x,'&sdate=1970-01-01&edate=2015-12-31&elems=pcpn&meta=ll\' > /scratch/mentors/dbuckmas/json_files/', x,'.json', sep = ''))
@@ -16,3 +17,9 @@ sapply(fips_code, pcpn_command)
 
 avgt_command <- function(x) system(paste('curl \'http://data.rcc-acis.org/MultiStnData?county=',x,'&sdate=1970-01-01&edate=2015-12-31&elems=avgt&meta=ll\' > /scratch/mentors/dbuckmas/avgt_files/', x,'.json', sep = ''))
 sapply(fips_code, avgt_command)
+
+maxt_command <- function(x) system(paste('curl \'http://data.rcc-acis.org/MultiStnData?county=',x,'&sdate=1970-01-01&edate=2015-12-31&elems=maxt&meta=ll\' > /scratch/mentors/dbuckmas/maxt_files/', x,'.json', sep = ''))
+sapply(fips_code, maxt_command)
+
+gdd32_command <- function(x) system(paste('curl \'http://data.rcc-acis.org/MultiStnData?county=',x,'&sdate=1970-01-01&edate=2015-12-31&elems=gdd32&meta=ll\' > /scratch/mentors/dbuckmas/gdd32_files/', x,'.json', sep = ''))
+sapply(fips_code, gdd32_command)
