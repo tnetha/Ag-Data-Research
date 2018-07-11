abeef3f5a2e39ab847459790c98d4ecbc81f39b7
tydor73 <3710608+tydor73@users.noreply.github.com>
2018-07-11 11:13:47 -0400

    deleted line that was causing install error

--- NOAAcounty/R/state_fips.R
+++ NOAAcounty/R/state_fips.R
@@ -2,4 +2,3 @@ state_fips <- function(states) {
   fips_code <- readRDS('https://www.dropbox.com/s/eurfbus8jwd7o8u/fips_code.rds?dl=0')
   fips_code$FIPS[fips_code$State %in% states]
 }
-NOAAcounty::state_fips('TX')
