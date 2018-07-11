cb8b0b9a473888683807c433f265fbe31d7e2307
tydor73 <3710608+tydor73@users.noreply.github.com>
2018-07-11 11:20:43 -0400

    Change url in state_fips

--- NOAAcounty/R/state_fips.R
+++ NOAAcounty/R/state_fips.R
@@ -1,4 +1,4 @@
 state_fips <- function(states) {
-  fips_code <- readRDS('https://www.dropbox.com/s/eurfbus8jwd7o8u/fips_code.rds?dl=0')
+  fips_code <- readRDS(gzcon(url('https://github.com/tnetha/Ag-Data-Research/blob/master/Data%20Sources/fips_code.rds?raw=true')))
   fips_code$FIPS[fips_code$State %in% states]
 }
